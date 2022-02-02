ea39c30cc8ca4ea06ba0d60f785b12239a6923ac
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-02-01 22:02:11 -0500

    All Functionality Done

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
-console.log("TEST ERIC");
+// console.log("TEST ERIC");
 
-var myButton = document.querySelector(".EricButton");
+// var myButton = document.querySelector(".EricButton");
 
-myButton.addEventListener("click", changeText);
-myButton.innerHTML = "START";
-var likeCount = 0;
+// myButton.addEventListener("click", changeText);
+// myButton.innerHTML = "START";
+// var likeCount = 0;
 
-function changeText() {
-    likeCount++;
-    myButton.innerHTML = likeCount;
+// function changeText() {
+//     console.log("Function Called")
+    
+//     likeCount++;
+//     myButton.innerHTML = likeCount;
+// }
+
+var loginOptions = ["Login","Logout"]
+var loginNumber = 0;
+var loginButton = document.querySelector("#login");
+function buttonChange(){
+
+    if (loginNumber === 0){
+        loginNumber++;
+    }else{
+        loginNumber--;
+    }
+    loginButton.innerHTML = loginOptions[loginNumber];
+}
+
+function removeButton(){
+    var elem = document.getElementById('addDef')
+    elem.parentNode.removeChild(elem)
+}
+
+function likeAlert(){
+    alert("Ninja Was Liked");
 }
